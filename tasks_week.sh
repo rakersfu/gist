8747e424b4fbb0f75d79960ca481edee4fc51172
rakersfu <134179858+rakersfu@users.noreply.github.com>
2025-11-16 17:27:03 +0800

    Update tasks_week.sh

--- tasks_week.sh
+++ tasks_week.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 set -e
 
-#find /tmp/navpage -type f > /tmp/navpage/navpage.txt
-cat /etc/cron.d/root-cron | tee /tmp/navpage/root-cron.txt > /dev/null
+#更新txt_to_html.sh
+curl -s "https://raw.githubusercontent.com/rakersfu/gist/main/txt_to_html.sh" -o "$APP_HOME/txt_to_html.sh" \
+  && log "INFO" "txt_to_html.sh 下载成功，保存到 $APP_HOME/txt_to_html.sh" \
+  || log "ERROR" "txt_to_html.sh 下载失败"
+#su - appuser -c "bash /tmp/txt_to_html.sh"
+
+#更新del_log.sh
+curl -s "https://raw.githubusercontent.com/rakersfu/gist/main/del_log.sh" -o "$APP_HOME/del_log.sh" \
+  && log "INFO" "del_log.sh 下载成功，保存到 $APP_HOME/del_log.sh" \
+  || log "ERROR" "del_log.sh 下载失败"
+#su - appuser -c "bash /tmp/del_log.sh"
